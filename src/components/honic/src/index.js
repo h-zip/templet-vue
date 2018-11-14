b346957a05f94bd32f6e4e35afa4cf52521d478a
hujianhao <hujh@hansintelligent.com>
2018-11-14 18:41:38 +0800

    change

--- src/components/honic/src/index.js
+++
@@ -1,9 +0,0 @@
-const components = {};
-const install = function (Vue, options) {
-  Object.keys(components).forEach(key => {
-    Vue.component(key, components[key]);
-  });
-};
-export default {
-  install
-};
