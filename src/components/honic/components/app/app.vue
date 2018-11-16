b42f956f2479a8829d90623276faabcd78ab8557
hujianhao <hujh@hansintelligent.com>
2018-11-16 15:12:11 +0800

    change

---
+++ src/components/honic/components/app/app.vue
@@ -0,0 +1,50 @@
+<template>
+  <div :class="classes">
+    <div class="navibar">top</div>
+    <div class="content">
+      aaa<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      aaa<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      aaa<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      aaa
+    </div>
+    <div class="tabbar">bottom</div>
+  </div>
+</template>
+
+<script>
+const prefixCls = 'xxx-app';
+export default {
+  name: "app",
+  data: function () {
+    return {
+      classes: [
+        `${prefixCls}`
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .navibar {
+    width: 100%;
+    position: fixed;
+    height: 45px;
+    top: 0;
+    background-color: #4cd964;
+  }
+  .tabbar {
+    width: 100%;
+    position: fixed;
+    height: 45px;
+    bottom: 0;
+    background-color: #9e9e9e;
+    box-sizing: content-box;
+    padding-bottom: env(safe-area-inset-bottom);
+    border: red 1px solid;
+  }
+  .content {
+    padding: 45px 0;
+    color: white;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import App from './App.vue'
-import router from './router/index'
-import Vonic from './components/vonic/index'
+// import VueRouter from 'vue-router'
+import './components/honic/styles/index.less';
+import App from './components/honic/components/app'
+// import router from './router/index'
+// import Vonic from './components/vonic/index'
 Vue.config.productionTip = false
-Vue.use(VueRouter)
-Vue.use(Vonic)
-Vue.use(Vonic.app, {
-  router,
-  pushMehod: 'push', // push, replace
-  pageTransition: 'ios'
-})
-// let app = new Vue({
-//   el: '#app',
+// Vue.use(VueRouter)
+// Vue.use(Vonic)
+// Vue.use(Vonic.app, {
 //   router,
-//   components: { App },
-//   template: '<App/>'
+//   pushMehod: 'push', // push, replace
+//   pageTransition: 'ios'
 // })
-// Vue.use(app)
+let app = new Vue({
+  el: '#app',
+  // router,
+  render: h=> h(App)
+})
